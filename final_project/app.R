a606c43fd3d583c79675cd8e5274447693d814fd
Isheka Agarwal <iagarwal@college.harvard.edu>
2019-11-20 23:28:11 -0500

    commiting the draft of shiny

---
+++ final_project/app.R
@@ -0,0 +1,48 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(janitor)
+library(ggplot2)
+library(purrr)
+library(tidyverse)
+library(readr)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    navbarPage(
+        "How Climate Change Affects Mental Illness?",
+        tabPanel(
+            title = "Introduction",
+            h6("By Isheka Agarwal"),
+            h3("About the project:"),
+            p("Climate change and mental illness are two of my major interests. Since the junior year of my high school, I have been very passionate about sustainability which crosses the path of climate change. A lot of people has a lot of assumptions about climate change. Some people are very concerned about it and some people think it is a hoax. Therefore, I thought about doing my Gov 1005 final project on how climate change affects mental illness. Firstly, I am analyzing how geographical locations affect the opinions of climate change. Then I am going to analyze the trends between climate change and mental illness.")
+        ),
+        
+        tabPanel(
+            title = "Graphics")
+)
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
