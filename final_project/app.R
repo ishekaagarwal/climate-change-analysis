8c8014d1012a0a350faadc0ba7dd868ddf10a9f6
Isheka Agarwal <iagarwal@college.harvard.edu>
2019-12-04 23:11:05 -0500

    committing work-in-progress of the project

--- final_project/app.R
+++ final_project/app.R
@@ -13,6 +13,7 @@ library(janitor)
 library(ggplot2)
 library(shinythemes)
 library(purrr)
+library(usmap)
 library(tidyverse)
 library(readr)
 
@@ -21,34 +22,101 @@ main2 <- read_rds("main2.rds")
 main3 <- read_rds("main3.rds")
 main4 <- read_rds("main4.rds")
 
+heat_2013 <- read_rds("heat_2013.rds")
+heat_2014 <- read_rds("heat_2014.rds")
+heat_2015 <- read_rds("heat_2015.rds")
+heat_2016 <- read_rds("heat_2016.rds")
+
+hospitalization_2013 <- read_rds("hospitalization2013.rds")
+hospitalization_2014 <- read_rds("hospitalization2014.rds")
+hospitalization_2015 <- read_rds("hospitalization2015.rds")
+hospitalization_2016 <- read_rds("hospitalization2016.rds")
+hospitalization_2017 <- read_rds("hospitalization2017.rds")
+
+prediction_2020 <- read_rds("prediction2020.rds")
+prediction_2025 <- read_rds("prediction2025.rds")
+prediction_2030 <- read_rds("prediction2030.rds")
+
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("superhero"),
     
     navbarPage(
-       title = "How Climate Change Affects Mental Illness?",
+       title = "Perspective on Historical and Future Consequences of Climate Change ",
         tabPanel(
-            title = "Introduction",
-            h6("By Isheka Agarwal"),
-            h3("About the project:"),
-            p("Climate change and mental illness are two of my major interests. Since the junior year of my high school, I have been very passionate about sustainability which crosses the path of climate change. A lot of people has a lot of assumptions about climate change. Some people are very concerned about it and some people think it is a hoax. Therefore, I thought about doing my Gov 1005 final project on how climate change affects mental illness. Firstly, I am analyzing how geographical locations affect the opinions of climate change. Then I am going to analyze the trends between climate change and mental illness.")
-        ),
+            title = "About the Project",
+            h5("I am Isheka Agarwal, a freshman at Harvard College. I am interested in Data Science, Applied Math and Psychology."),
+            br(),
+            h4("Background of the Project:"),
+            h5("For my data analysis class (GOV 1005), I made this final project. This project analyzes the historical consequences of climate change, represents future projection for consequences of climate change and various opinions of people living in various regions in the United States. This project also creates a model to analyze what affects people's concern about climate change."),
+            h5("The historical consequences of climate change are indicated by extreme heat days and events by counties in the United States and measured by number of extreme heat days occuring from 2013-16. The extreme heat days are counted when the daily maximum temperature goes beyond a relative threshold of 90th Percentile. In order to see how extreme heat days affect people living in these regions, I analyzed the heat stress hospitalizations in each of the states, mapping the number of hospitalizations for heat stress across the United States from 2013-17. Then I analyzed relative data to predict the future projection of extreme heat across the United States within the next decade and how it will impact people's lives."),
+            h5("I collected data from National Environmental Public Health Tracking Network to make my analysis on abovementioned areas."),
+            h5("Additionally, I accessed data from the Climate Change in the American Mind (CCAM) research project to analyze the climate change beliefs of people who are living in four major regions across the United States: Northeast, South, Midwest and West. Then I created some models using linear regression to analyze what affects people's beliefs about climate change."), 
+            h5("Github repository for this project: https://github.com/ishekaagarwal/final-project-isheka"),
+            h6("Accessed from:
+               Centers for Disease Control and Prevention. Environmental Health Tracking Program. Extreme heat days and events. https://ephtracking.cdc.gov/DataExplorer.
+               Yale Program on Climate Change Communication (YPCCC) & George Mason University Center for Climate Change Communication (Mason 4C). (2019). Climate Change in the American Mind: National survey data on public opinion (2008-2017) [Data file and codebook]. doi: 10.17605/OSF.IO/W36GN")
+            ),
         
         tabPanel(
-            title = "Graphics",
-            h3("How living in different regions affect people's perspective on climate change?"),
+            title = "Consequences of Climate Change",
+            h5("Heat Maps of Heat Day Events, Hospitalizations and Future Prediction"),
             br(),
             br(),
             sidebarLayout(
                 sidebarPanel(
-                    radioButtons(inputId = "region_type",
-                                 label = "US Demographical region", 
-                                 choices = c("Northeast", "West", "South", "Midwest"))
+                    radioButtons(inputId = "heat_year",
+                                 label = "Select a year", 
+                                 choices = c("2013", "2014", "2015", "2016"))
                 ),
+            mainPanel(
+                plotOutput("heatchart")
+            )), 
+            br(),
+            br(),
+            br(),
+            br(),
+            sidebarLayout(
+                sidebarPanel(
+                    radioButtons(inputId = "hospitalization_year",
+                                 label = "Select a year", 
+                                 choices = c("2013", "2014", "2015", "2016", "2017"))
+                ),
+                mainPanel(
+                    plotOutput("hospitalizationchart")
+                )
+            ), 
+            br(),
+            br(),
+            br(),
+            br(),
+            sidebarLayout(
+                sidebarPanel(
+                    radioButtons(inputId = "prediction_year",
+                                 label = "Select a year", 
+                                 choices = c("2020", "2025", "2030"))
+                ),
+                mainPanel(
+                    plotOutput("predictionchart")
+                )
+            )
+    ),  tabPanel(
+        title = "Climate Change Beliefs",
+        h5("People's beliefs about climate change in four different regions in the United States"),
+        br(),
+        br(),
+        sidebarLayout(
+            sidebarPanel(
+                radioButtons(inputId = "region_type",
+                             label = "US Demographical region", 
+                             choices = c("Northeast", "West", "South", "Midwest"))
+            ),
             mainPanel(
                 plotOutput("regionchart")
             )
-            )
-)))
+        )
+    )
+))
 
 # Define server logic required to draw a histogram
 
@@ -67,6 +135,52 @@ server <- function(input, output) {
         }
         else if(input$region_type == "Midwest"){
             main4
+        }
+    }) 
+    
+    output$heatchart <- renderPlot({
+        if(input$heat_year == "2013"){
+            heat_2013
+        }
+        else if(input$heat_year == "2014"){
+            heat_2014
+        }
+        else if(input$heat_year == "2015"){
+            heat_2015
+        }
+        else if(input$heat_year == "2016"){
+            heat_2016
+        }
+    })
+    
+    
+    output$hospitalizationchart <- renderPlot({
+        if(input$hospitalization_year == "2013"){
+            hospitalization_2013
+        }
+        else if(input$hospitalization_year == "2014"){
+            hospitalization_2014
+        }
+        else if(input$hospitalization_year == "2015"){
+            hospitalization_2015
+        }
+        else if(input$hospitalization_year == "2016"){
+            hospitalization_2016
+        }
+        else if(input$hospitalization_year == "2017"){
+            hospitalization_2017
+        }
+    })
+    
+    output$predictionchart <- renderPlot({
+        if(input$prediction_year == "2020"){
+            prediction_2020
+        }
+        else if(input$prediction_year == "2025"){
+            prediction_2025
+        }
+        else if(input$prediction_year == "2030"){
+            prediction_2030
         }
     })
 }
--- prep.R
+++ prep.R
@@ -9,7 +9,11 @@ library(scales)
 library(stringr)
 library(broom)
 library(infer)
+library(usmap)
 library(ggridges)
+library(tidyr)
+library(dplyr)
+library(data.table)
 library(tidyverse)
 
 # creating directory and reading in the data 
@@ -19,7 +23,13 @@ dataset <- read.spss("raw-data/CCAM_SPSS_Data.sav", to.data.frame=TRUE)
 
 dataset2 <- read.csv("raw-data/PCS_2015.csv")
 
-#Processing the data for the first (northeast) ggplot
+dataset3 <- read.csv("raw-data/data_no_of_extreme_heat_days.csv")
+
+dataset4 <- read.csv("raw-data/data_no_of_hospitalizations.csv")
+
+dataset5 <- read.csv("raw-data/data_prediction.csv")
+
+#Processing the data for how corcerned people living in northeast are 
 
 main <- dataset %>%
   drop_na() %>%
@@ -33,12 +43,15 @@ main <- dataset %>%
   mutate(n = n * 100/ total) %>% 
   select(-total) %>% 
   ggplot(aes(x = worry, y = n)) + 
-  geom_col() + 
-  coord_flip()
+  geom_col(fill = "white", color = "red") + 
+  coord_flip() + 
+  labs(title = "How concerned people living in northeastern region of the United States are about climate change?",
+       y = "Percentage of people",
+       x = "Level of concerns")
 
 write_rds(main, "final_project/main.rds")
 
-#Processing the data for the second (west) ggplot
+#Processing the data for how corcerned people living in west are
 
 main2 <- dataset %>%
   drop_na() %>%
@@ -52,12 +65,15 @@ main2 <- dataset %>%
   mutate(n = n * 100/ total) %>% 
   select(-total) %>% 
   ggplot(aes(x = worry, y = n)) + 
-  geom_col() + 
-  coord_flip()
+  geom_col(fill = "white", color = "red") + 
+  coord_flip() + 
+  labs(title = "How concerned people living in western region of the United States are about climate change?",
+       y = "Percentage of people",
+       x = "Level of concerns")
 
 write_rds(main2, "final_project/main2.rds")
 
-#Processing the data for the third (south) ggplot 
+#Processing the data for how corcerned people living in south are
 
 main3 <- dataset %>%
   drop_na() %>%
@@ -71,12 +87,15 @@ main3 <- dataset %>%
   mutate(n = n * 100/ total) %>% 
   select(-total) %>% 
   ggplot(aes(x = worry, y = n)) + 
-  geom_col() + 
-  coord_flip()
+  geom_col(fill = "white", color = "red") + 
+  coord_flip() + 
+  labs(title = "How concerned people living in the southern region of the United States are about climate change?",
+       y = "Percentage of people",
+       x = "Level of concerns")
 
 write_rds(main3, "final_project/main3.rds")
 
-#Processing the data for the fourth (Midwest) ggplot
+#Processing the data for how corcerned people living in midwest are
 
 main4 <- dataset %>%
   drop_na() %>%
@@ -90,8 +109,233 @@ main4 <- dataset %>%
   mutate(n = n * 100/ total) %>% 
   select(-total) %>% 
   ggplot(aes(x = worry, y = n)) + 
-  geom_col() + 
-  coord_flip()
+  geom_col(fill = "white", color = "red") + 
+  coord_flip() + 
+  labs(title = "How concerned people living in midwest of the United States are about climate change?",
+       y = "Percentage of people",
+       x = "Level of concerns")
 
 write_rds(main4, "final_project/main4.rds")
+
+
+# Processing the data for number of extreme heat days and events by state
+
+    ## For 2013
+
+heat_2013 <- dataset3 %>%
+  filter(Year == "2013") 
+
+heat_days_2013 <- plot_usmap(data = heat_2013, values = "Value", 
+                             regions = "states", size = 0.05) +
+  theme(panel.background = element_rect(color = "black", fill = "white")) +
+  scale_fill_continuous(low = "yellow", high = "red", name = "Rel. Prob.") +
+  labs(title = "Extreme Heat Days and Events in the United States in 2013",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(heat_days_2013, "final_project/heat_2013.rds")
+
+
+  ## For 2014
+
+
+heat_2014 <- dataset3 %>%
+  filter(Year == "2014") 
+heat_days_2014 <- plot_usmap(data = heat_2014, values = "Value", regions = "states", 
+             size = 0.05) +
+  theme(panel.background = element_rect(color = "black", fill = "white")) +
+  scale_fill_continuous(low = "yellow", high = "red", name = "Rel. Prob.") +
+  labs(title = "Extreme Heat Days and Events in the United States in 2014",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(heat_days_2014, "final_project/heat_2014.rds")
+
+
+  ## For 2015 
+
+heat_2015 <- dataset3 %>%
+  filter(Year == "2015")
+
+heat_days_2015 <- plot_usmap(data = heat_2015, values = "Value", regions = "states", 
+             size = 0.05) +
+  theme(panel.background = element_rect(color = "black", fill = "white")) +
+  scale_fill_continuous(low = "yellow", high = "red", name = "Rel. Prob.") +
+  labs(title = "Extreme Heat Days and Events in the United States in 2015",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(heat_days_2015, "final_project/heat_2015.rds")
+
+
+  ## For 2016
+
+heat_2016 <- dataset3 %>%
+  filter(Year == "2016") 
+
+heat_days_2016 <- plot_usmap(data = heat_2016, values = "Value", regions = "states", 
+             size = 0.05) +
+  theme(panel.background = element_rect(color = "black", fill = "white")) +
+  scale_fill_continuous(low = "yellow", high = "red", name = "Rel. Prob.") +
+  labs(title = "Extreme Heat Days and Events in the United States in 2016",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(heat_days_2016, "final_project/heat_2016.rds")
+
+
+# Creating heat maps for number of hospitalizations 
+  ##For 2013
+
+names(dataset4)[1]<- "fips"
+
+hospitalization_2013 <- dataset4 %>%
+  filter(Year == "2013") 
+
+no_hospitalization_2013 <- plot_usmap(data = hospitalization_2013, 
+             values = "Value", 
+             regions = "state", 
+             size = 0.05) +
+  theme(panel.background = element_rect(color = "black", 
+                                        fill = "white")) +
+  scale_fill_continuous(low = "white", high = "blue", 
+                        name = "Density of hospitalizations") +
+  labs(title = "Heat Stress Hospitalizations in the United States in 2013",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(no_hospitalization_2013, "final_project/hospitalization2013.rds")
+
+  ##For 2014
+
+hospitalization_2014 <- dataset4 %>%
+  filter(Year == "2014") 
+
+no_hospitalization_2014 <- plot_usmap(data = hospitalization_2014, 
+                                      values = "Value", 
+                                      regions = "state", 
+                                      size = 0.05) +
+  theme(panel.background = element_rect(color = "black", 
+                                        fill = "white")) +
+  scale_fill_continuous(low = "white", high = "blue", 
+                        name = "Density of hospitalizations") +
+  labs(title = "Heat Stress Hospitalizations in the United States in 2014",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(no_hospitalization_2014, "final_project/hospitalization2014.rds")
+
+  ## For 2015
+
+hospitalization_2015 <- dataset4 %>%
+  filter(Year == "2015") 
+
+no_hospitalization_2015 <- plot_usmap(data = hospitalization_2015, 
+                                      values = "Value", 
+                                      regions = "state", 
+                                      size = 0.05) +
+  theme(panel.background = element_rect(color = "black", 
+                                        fill = "white")) +
+  scale_fill_continuous(low = "white", high = "blue", 
+                        name = "Density of hospitalizations") +
+  labs(title = "Heat Stress Hospitalizations in the United States in 2015",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(no_hospitalization_2015, "final_project/hospitalization2015.rds")
+
+  ## For 2016
+
+hospitalization_2016 <- dataset4 %>%
+  filter(Year == "2016") 
+
+no_hospitalization_2016 <- plot_usmap(data = hospitalization_2016, 
+                                      values = "Value", 
+                                      regions = "state", 
+                                      size = 0.05) +
+  theme(panel.background = element_rect(color = "black", 
+                                        fill = "white")) +
+  scale_fill_continuous(low = "white", high = "blue", 
+                        name = "Density of hospitalizations") +
+  labs(title = "Heat Stress Hospitalizations in the United States in 2016",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(no_hospitalization_2016, "final_project/hospitalization2016.rds")
+
+  ## For 2017
+
+hospitalization_2017 <- dataset4 %>%
+  filter(Year == "2017") 
+
+no_hospitalization_2017 <- plot_usmap(data = hospitalization_2017, 
+                                      values = "Value", 
+                                      regions = "state", 
+                                      size = 0.05) +
+  theme(panel.background = element_rect(color = "black", 
+                                        fill = "white")) +
+  scale_fill_continuous(low = "white", high = "blue", 
+                        name = "Density of hospitalizations") +
+  labs(title = "Heat Stress Hospitalizations in the United States in 2017",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(no_hospitalization_2017, "final_project/hospitalization2017.rds")
+
+
+# Creating heat maps for future projection of extreme heat day events 
+  ## For 2020
+
+prediction_2020 <- dataset5 %>%
+  filter(Year == "2020") 
+
+heat_prediction_2020 <- plot_usmap(data = prediction_2020, 
+                                      values = "Value", 
+                                      regions = "state", 
+                                      size = 0.05) +
+  theme(panel.background = element_rect(color = "black", 
+                                        fill = "white")) +
+  scale_fill_continuous(low = "white", high = "orange", 
+                        name = "Projected no. of future events") +
+  labs(title = "Future Projection of Extreme Heat Days and Events in the United States in 2020",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(heat_prediction_2020, "final_project/prediction2020.rds")
+
+  ## For 2025
+
+prediction_2025 <- dataset5 %>%
+  filter(Year == "2025") 
+
+heat_prediction_2025 <- plot_usmap(data = prediction_2025, 
+                                   values = "Value", 
+                                   regions = "state", 
+                                   size = 0.05) +
+  theme(panel.background = element_rect(color = "black", 
+                                        fill = "white")) +
+  scale_fill_continuous(low = "white", high = "orange", 
+                        name = "Projected no. of future events") +
+  labs(title = "Future Projection of Extreme Heat Days and Events in the United States in 2025",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(heat_prediction_2025, "final_project/prediction2025.rds")
+
+  ## For 2030
+
+prediction_2030 <- dataset5 %>%
+  filter(Year == "2030") 
+
+heat_prediction_2030 <- plot_usmap(data = prediction_2030, 
+                                   values = "Value", 
+                                   regions = "state", 
+                                   size = 0.05) +
+  theme(panel.background = element_rect(color = "black", 
+                                        fill = "white")) +
+  scale_fill_continuous(low = "white", high = "orange", 
+                        name = "Projected no. of future events") +
+  labs(title = "Future Projection of Extreme Heat Days and Events in the United States in 2030",
+       caption = "Data collected from National Environmental Public Health Tracking Network.")
+
+write_rds(heat_prediction_2030, "final_project/prediction2030.rds")
+
+
+
+
+  #merge(x, y, by.x = "fips", by.y = "FIPS", all.x = T)
+
+  #run regression on number of heat days vs. population size 
+
+  #run regression on number of heat days by county vs urban status 
+
 
